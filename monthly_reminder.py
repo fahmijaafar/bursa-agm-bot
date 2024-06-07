7a515bf9bcec9dbfccaf6d4a6364dd5c46213235
Azuan Alias <azuanalias92@gmail.com>
2024-06-08 01:50:42 +0800

    Refactor : update using proper markdown and prettytable

--- monthly_reminder.py
+++ monthly_reminder.py
@@ -5,9 +5,15 @@ import os
 TOKEN = os.environ['TOKEN']
 CHAT_ID = os.environ['CHAT_ID']
 
-base_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text="
-
 # Every 1st day of the month, at 7AM
-
 monthly_reminder = monthly_stocks_to_buy()
-requests.get(base_url+monthly_reminder)
+
+# Send the message to Telegram
+send_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+params = {
+    "chat_id": CHAT_ID,
+    "text": monthly_reminder,
+    "parse_mode": "Markdown"
+}
+response = requests.post(send_url, data=params)
+print(response.json())
