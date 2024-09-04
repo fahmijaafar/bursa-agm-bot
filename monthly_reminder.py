178dcb4b8e53faaa5af954e9de4d4cd76858082e
Fahmi Jaafar <54757196+fahmijaafar@users.noreply.github.com>
2024-09-04 14:17:06 +0800

    Update monthly_reminder.py

--- monthly_reminder.py
+++ monthly_reminder.py
@@ -7,13 +7,14 @@ CHAT_ID = os.environ['CHAT_ID']
 
 # Every 1st day of the month, at 7AM
 monthly_reminder = monthly_stocks_to_buy()
-
-# Send the message to Telegram
-send_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-params = {
-    "chat_id": CHAT_ID,
-    "text": monthly_reminder,
-    "parse_mode": "Markdown"
-}
-response = requests.post(send_url, data=params)
-print(response.json())
+if monthly_reminder != 0:
+    
+    # Send the message to Telegram
+    send_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    params = {
+        "chat_id": CHAT_ID,
+        "text": monthly_reminder,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(send_url, data=params)
+    print(response.json())
